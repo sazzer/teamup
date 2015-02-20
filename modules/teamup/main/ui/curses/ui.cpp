4e83997c630548a952954105c389ab6ebf8b6d13
Graham Cox <graham.cox@oclc.org>
2015-02-20 12:24:12 +0000

    Print something

--- modules/teamup/main/ui/curses/ui.cpp
+++ modules/teamup/main/ui/curses/ui.cpp
@@ -10,8 +10,27 @@ namespace Teamup {
             public:
                 CursesUI() {
                     LOG(DEBUG) << "Starting Curses UI";
+                    // Do all of the initial setup
                     initscr();
+                    keypad(stdscr, TRUE);
+                    nonl();
+                    cbreak();
+                    echo();
+                    if (has_colors()) {
+                        start_color();
+
+                        init_pair(1, COLOR_RED,     COLOR_BLACK);
+                        init_pair(2, COLOR_GREEN,   COLOR_BLACK);
+                        init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
+                        init_pair(4, COLOR_BLUE,    COLOR_BLACK);
+                        init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+                        init_pair(6, COLOR_CYAN,    COLOR_BLACK);
+                        init_pair(7, COLOR_WHITE,   COLOR_BLACK);
+                    }
+
                     clear();
+                    attrset(COLOR_PAIR(5));
+                    mvprintw(1, 1, "Hello, World!");
                     refresh();
                 }
                 ~CursesUI() {
